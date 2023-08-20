e37b64b3f68dd3ee47cfe7efbeb63f854bd87842
ivyfu77 <ivy.fu@lightspeedhq.com>
2023-08-20 12:29:05 +1200

    Basic UI and styling

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,28 @@ import { Button } from './components/common'
 
 const App: React.FC<{}> = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Reset Your Password
-        </p>
-        <Button>
-          Reset
-        </Button>
-      </header>
+    <div className="App-container">
+      <div className="reset-password">
+        <header className="header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Reset Your Password
+          </p>
+        </header>
+        <div className="password-container">
+          <label htmlFor="new-pass">New Password:</label>
+          <input type="password" id="new-pass" name="password" minLength={8} required />
+        </div>
+        <div className="password-container">
+          <label htmlFor="confirm-pass">Confirm Password:</label>
+          <input type="password" id="confirm-pass" name="password" minLength={8} required />
+        </div>
+        <div className="reset-btn-container">
+          <Button>
+            Reset
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
