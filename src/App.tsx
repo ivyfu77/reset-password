eeb84c985d30c1e4911cf96395621260095cbbf6
ivyfu77 <ivy.fu@lightspeedhq.com>
2023-08-20 17:01:05 +1200

    Set conditions object to mark the validation result; Show Notifications to remind user

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,91 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from './imgs/logo.svg'
 import './style/App.css'
 
 import { Button } from './components/common'
 import { NewPassword } from './components/view/NewPassword'
 
-const ERRORS = {
-  EMPTY: 'empty',
-  LESS_THAN_MIN_LENGTH: 'lessThanMinLength',
-  NO_DIGIT: 'noDigit',
-  NO_TWO_SPECIAL_CHAR: 'noTwoSpecialChar',
-}
+// const ERRORS = {
+//   EMPTY: 'empty',
+//   LESS_THAN_MIN_LENGTH: 'lessThanMinLength',
+//   NO_DIGIT: 'noDigit',
+//   NO_TWO_SPECIAL_CHAR: 'noTwoSpecialChar',
+// }
 
 const REGEX = {
   AT_LEAST_ONE_DIGIT: /\d/,
   AT_LEAST_TWO_SPECIAL_CHAR: /(?=(.*[`!@#$%^&*\-_=+'/.,]){2})/
 }
 
-const hasError = (value: string): string | boolean => {
-  if (!value) {
-    return ERRORS.EMPTY
-  }
-  if (value.length > 15) {
-    return false
-  }
-  if (value.length < 8) {
-    return ERRORS.LESS_THAN_MIN_LENGTH
-  }
-  if (!value.match(REGEX.AT_LEAST_ONE_DIGIT)) {
-    return ERRORS.NO_DIGIT
-  }
-  if (!value.match(REGEX.AT_LEAST_TWO_SPECIAL_CHAR)) {
-    return ERRORS.NO_TWO_SPECIAL_CHAR
-  }
-
-  return false
+export type MatchConditions = {
+  notEmpty: boolean
+  minLength: boolean
+  oneDigit: boolean
+  twoSpecialChar: boolean
+  longEnough: boolean // more than 15 characters
 }
 
 const App: React.FC<{}> = () => {
-  const [password, setPassword] = useState<string>('')
+  const [hasError, setHasError] = useState<boolean>(true)
+  const [conditions, setConditions] = useState<MatchConditions>({
+    notEmpty: false,
+    minLength: false,
+    oneDigit: false,
+    twoSpecialChar: false,
+    longEnough: false
+  })
+
+  const resetConditions = (key: string, value: boolean) => {
+    if (conditions[key as keyof MatchConditions] !== value) {
+      setConditions({
+        ...conditions,
+        [key]: value
+      })
+    }
+  }
+
+  const updateConditions = (value: string) => {
+    if (!value) {
+      resetConditions('notEmpty', false)
+    } else {
+      resetConditions('notEmpty', true)
+    }
+
+    if (value.length > 15) {
+      resetConditions('longEnough', true)
+    } else {
+      resetConditions('longEnough', false)
+    }
+
+    if (value.length < 8) {
+      resetConditions('minLength', false)
+    } else {
+      resetConditions('minLength', true)
+    }
+
+    if (!value.match(REGEX.AT_LEAST_ONE_DIGIT)) {
+      resetConditions('oneDigit', false)
+    } else {
+      resetConditions('oneDigit', true)
+    }
+
+    if (!value.match(REGEX.AT_LEAST_TWO_SPECIAL_CHAR)) {
+      resetConditions('twoSpecialChar', false)
+    } else {
+      resetConditions('twoSpecialChar', true)
+    }
+  }
+
+  const checkError = (conditions: MatchConditions) => {
+    return !conditions.longEnough && Object.keys(conditions).some(key => {
+      const condition = conditions[key as keyof MatchConditions]
+      return key !== 'longEnough' && !condition
+    })
+  }
+
+  useEffect(() => {
+    setHasError(checkError(conditions))
+  }, [conditions])
 
   return (
     <div className="App-container">
@@ -49,17 +96,19 @@ const App: React.FC<{}> = () => {
             Reset Your Password
           </p>
         </header>
-        <NewPassword onChange={(e) => {
-          const value = e.target.value
-          //console.log(value)
-          setPassword(value)
-        }} />
+        <NewPassword
+          onChange={(password) => {
+            console.log(conditions)
+            updateConditions(password)
+          }}
+          conditions={conditions}
+        />
         {/* <div className="password-container">
           <label htmlFor="confirm-pass">Confirm Password:</label>
           <input type="password" id="confirm-pass" name="password" minLength={8} required />
         </div> */}
         <div className="reset-btn-container">
-          <Button disabled={Boolean(hasError(password))}>
+          <Button disabled={hasError}>
             Reset
           </Button>
         </div>
--- src/components/view/NewPassword.tsx
+++ src/components/view/NewPassword.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import classNames from 'classnames'
+import { MatchConditions } from '../../App'
 
 interface Props {
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  onChange: (password: string) => void
+  conditions: MatchConditions
 }
 
-export const NewPassword: React.FC<Props> = ({ onChange }) => {
+export const NewPassword: React.FC<Props> = ({ onChange, conditions }) => {
+  const [password, setPassword] = useState<string>('')
   return (
     <div className="password-container">
       <label htmlFor="new-pass">New Password:</label>
@@ -13,8 +17,28 @@ export const NewPassword: React.FC<Props> = ({ onChange }) => {
         id="new-pass"
         name="password"
         minLength={8}
-        onChange={onChange}
+        onChange={(e) => {
+          const value = e.target.value.trim()
+          setPassword(value)
+          onChange(value)
+        }}
         required />
+      <div className="notifications">
+        <span className="notifications_header">Your password should:</span>
+        <span className={classNames('notifications_msg', { 'satisefied': conditions.minLength})}>
+          {conditions.minLength ? '✓ ' : '⚠ '}At least 8 characters
+        </span>
+        <span className={classNames('notifications_msg', { 'satisefied': conditions.oneDigit})}>
+          {conditions.oneDigit ? '✓ ' : '⚠ '}At least 1 number
+        </span>
+        <span className={classNames('notifications_msg', { 'satisefied': conditions.twoSpecialChar})}>
+          {conditions.twoSpecialChar ? '✓ ' : '⚠ '}At least 2 special characters
+        </span>
+        <span className="notifications_header">Or:</span>
+        <span className={classNames('notifications_msg', { 'satisefied': password.length > 15})}>
+          {password.length > 15 ? '✓ ' : '⚠ '}More than 15 characters
+        </span>
+      </div>
     </div>
   )
 }
